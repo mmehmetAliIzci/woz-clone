1d107a43d035467248f5b2b3aa6451e959646e74
Mehmet Ali IZCI <mmehmetaliizci@hotmail.com>
2023-11-26 17:15:25 +0100

    fix: move types to global

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,7 +1,7 @@
 import 'little-state-machine';
 import '@emotion/react';
 import { AddressResponse } from '@api/address/route';
-import { Steps } from './src/stateManagers/types';
+import { Steps } from '../src/stateManagers/types';
 import { WozValueResponse } from '@api/woz-value/route';
 
 declare module 'little-state-machine' {
@@ -9,18 +9,5 @@ declare module 'little-state-machine' {
     selectedAddress?: AddressResponse;
     wozValues?: WozValueResponse;
     step?: Steps;
-  }
-}
-
-declare module '@emotion/react' {
-  export interface Theme {
-    colors: {
-      light: string;
-      primary: string;
-      primaryText: string;
-      secondary: string;
-      gray: string;
-      backgroundColor: string;
-    };
   }
 }
